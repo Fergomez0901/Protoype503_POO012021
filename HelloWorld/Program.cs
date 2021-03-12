64a083df3b2de8dde3163818c0cca578c81bc355
Luis Alexander Hernandez Martinez <00129020@uca.edu.sv>
2021-03-12 15:56:03 -0600

    changed the function add to subtraction

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -14,12 +14,12 @@ namespace HelloWorld
             Console.WriteLine("Enter a value for b:");
             b = Int32.Parse(Console.ReadLine()); 
 
-            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a, b)} "); 
+            Console.WriteLine($"Adding {a} + {b} equals {subtractNumbers(a, b)} "); 
         }
 
-        public static int AddNumbers(int a, int b)
+        public static int subtractNumbers(int a, int b)
         {
-            return a + b; 
+            return a - b; 
         }
     }
 }
